ae21b0d5fb31e856dfa98f52c7b4b1adea39ba28
Aekyung <isaiah6014915@gmail.com>
2023-11-01 16:48:10 +0900

    feat: mobile [day].tsx done

--- @app/cpapp/features/calendar/pet/[petId]/day/[day].tsx
+++ @app/cpapp/features/calendar/pet/[petId]/day/[day].tsx
@@ -1,6 +1,6 @@
 import CustomInput from "@app/cpapp/components/CustomInput";
 import { FourOhFour } from "@app/cpapp/components/FourOhFour";
-import { Text } from "@app/cpapp/design/typography";
+import { P, Text } from "@app/cpapp/design/typography";
 import { View } from "@app/cpapp/design/view";
 import { AuthRestrict, SharedLayout } from "@app/cpapp/layouts/SharedLayout";
 import { isSafe } from "@app/cpapp/utils/utils";
@@ -18,11 +18,15 @@ import {
 } from "@app/graphql";
 import { extractError, getCodeFromError } from "@app/lib";
 import fourOhFour from "@app/server/public/404_page.png";
+import bathroomStatusGray from "@app/server/public/bathroom_status_grey.png";
 import blueCaret from "@app/server/public/blue_caret.png";
 import calendar from "@app/server/public/calendar_blk.png";
-import logo from "@app/server/public/logo.png";
+import dietStatusGray from "@app/server/public/diet_status_grey.png";
+import healthStatusGray from "@app/server/public/health_status_grey.png";
+import playStatusGray from "@app/server/public/play_status_grey.png";
+import sleepStatusGray from "@app/server/public/sleep_status_grey.png";
+import walkingStatusGray from "@app/server/public/walking_status_grey.png";
 import { format, parseISO } from "date-fns";
-import { Field, Formik } from "formik";
 import { ScrollView } from "moti";
 import { StyledComponent } from "nativewind";
 import React, { FC, useCallback, useEffect, useState } from "react";
@@ -33,6 +37,96 @@ import { Link } from "solito/link";
 import { useRouter } from "solito/navigation";
 import { Button, Circle, Tooltip, useTheme } from "tamagui";
 
+const TestExpandable = ({
+  setIsExpandable,
+}: {
+  setIsExpandable: (isExpandable: boolean) => void;
+}) => {
+  useEffect(() => {
+    setIsExpandable(true);
+    return () => {
+      setIsExpandable(false);
+    };
+  });
+  return null;
+};
+
+type CommentRowProps = {
+  statusImage: any;
+  statusComment: string | null;
+};
+
+const CommentRow: FC<CommentRowProps> = ({
+  statusImage,
+  statusComment,
+}: CommentRowProps) => {
+  const [isCommentExpanded, setIsCommentExpanded] = useState(false);
+  const [isExpandable, setIsExpandable] = useState(false);
+
+  return (
+    <View className="mt-5 flex w-full flex-row items-start justify-between">
+      <View className="mr-[10px] flex w-[19.09px] flex-row items-center">
+        <StyledComponent
+          component={SolitoImage}
+          className="h-[20px] w-[19.09px]"
+          src={statusImage}
+          alt=""
+          // fill
+        />
+      </View>
+
+      <Text className="font-poppins mb-0 mt-[2px] w-full pr-[29.09px] text-start text-[12px] text-[#8F9092]">
+        {statusComment}
+      </Text>
+
+      {/* {isCommentExpanded ? (
+          <P
+            className="calendar-comment font-poppins text-pupcleGray mb-0 text-[16px] font-semibold"
+            key={`isExpanded-${true}`}
+          >
+            {statusComment} &nbsp;&nbsp;
+            {isCommentExpanded && (
+              <Button
+                className="border-pupcleBlue inline-block h-[20px] w-[35px] rounded-full border-[1px] p-0"
+                onClick={() => setIsCommentExpanded(false)}
+              >
+                <Text className="font-poppins text-pupcleBlue text-[10px] font-bold">
+                  접기
+                </Text>
+              </Button>
+            )}
+          </P>
+        ) : (
+          <P
+            key={`isExpanded-${false}`}
+            className="calendar-comment font-poppins mb-0 text-[12px] text-[#8F9092]"
+            // ellipsis={{
+            //   rows: 1,
+            //   expandable: true,
+            //   onExpand: () => setIsCommentExpanded(true),
+            //   symbol: <TestExpandable setIsExpandable={setIsExpandable} />,
+            // }}
+          >
+            {statusComment}
+          </P>
+        )} */}
+      <View>
+        {/* {!isExpandable || !statusComment?.length ? (
+          <View className="w-[44px]"></View>
+        ) : (
+          <Button
+            onClick={() => setIsCommentExpanded(true)}
+            className="bg-pupcleBlue flex h-[20px] w-[44px] items-center rounded-full border-none"
+          >
+            <Text className="font-poppins text-[10px] font-bold text-white">
+              펼치기
+            </Text>
+          </Button>
+        )} */}
+      </View>
+    </View>
+  );
+};
 interface CalendarPetDayScreenInnerProps {
   privateDailyRecord: CalendarRecords_PrivateDailyRecordFragment;
   status: DailyRecordStatus;
@@ -41,12 +135,46 @@ interface CalendarPetDayScreenInnerProps {
 const CalendarPetDayScreenInner: FC<CalendarPetDayScreenInnerProps> = ({
   privateDailyRecord,
   status,
-}) => {
+}: CalendarPetDayScreenInnerProps) => {
   return (
-    <View className="">
-      <Text>{privateDailyRecord.sleepStatus}</Text>
-      <Text>{privateDailyRecord.sleepComment}</Text>
-    </View>
+    <>
+      {status === privateDailyRecord.sleepStatus && (
+        <CommentRow
+          statusImage={sleepStatusGray}
+          statusComment={privateDailyRecord.sleepComment}
+        />
+      )}
+      {status === privateDailyRecord.dietStatus && (
+        <CommentRow
+          statusImage={dietStatusGray}
+          statusComment={privateDailyRecord.dietComment}
+        />
+      )}
+      {status === privateDailyRecord.walkingStatus && (
+        <CommentRow
+          statusImage={walkingStatusGray}
+          statusComment={privateDailyRecord.walkingComment}
+        />
+      )}
+      {status === privateDailyRecord.playStatus && (
+        <CommentRow
+          statusImage={playStatusGray}
+          statusComment={privateDailyRecord.playComment}
+        />
+      )}
+      {status === privateDailyRecord.bathroomStatus && (
+        <CommentRow
+          statusImage={bathroomStatusGray}
+          statusComment={privateDailyRecord.bathroomComment}
+        />
+      )}
+      {status === privateDailyRecord.healthStatus && (
+        <CommentRow
+          statusImage={healthStatusGray}
+          statusComment={privateDailyRecord.healthComment}
+        />
+      )}
+    </>
   );
 };
 
@@ -131,7 +259,7 @@ export function CalendarPetDayScreen() {
                   bad
                 </Text>
                 <CalendarPetDayScreenInner
-                  status={DailyRecordStatus.Good}
+                  status={DailyRecordStatus.Bad}
                   privateDailyRecord={dailyRecord}
                 />
               </View>
