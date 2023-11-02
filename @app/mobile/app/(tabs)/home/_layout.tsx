9ce4ea512b3c052a91ee80924b8608f177b058ac
Aekyung <isaiah6014915@gmail.com>
2023-11-02 16:07:19 -0400

    feat: mobile tabs with drawer

---
+++ @app/mobile/app/(tabs)/home/_layout.tsx
@@ -0,0 +1,23 @@
+import { Drawer } from "expo-router/drawer";
+import React from "react";
+
+export default function Layout() {
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Home",
+          title: "overview",
+        }}
+      />
+      {/* <Drawer.Screen
+        name="user/[id]" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "User",
+          title: "overview",
+        }}
+      /> */}
+    </Drawer>
+  );
+}
