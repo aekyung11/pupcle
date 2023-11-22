71e821dc3ec5e42bcea7def75c37cf11859d0a70
Aekyung <isaiah6014915@gmail.com>
2023-11-23 02:31:19 +0900

    feat: add focused on tab bar for mobile

--- @app/mobile/app/(tabs)/_layout.tsx
+++ @app/mobile/app/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
 import { Text } from "@app/cpapp/design/typography";
 import { View } from "@app/cpapp/design/view";
 import tabAccount from "@app/server/public/tab_account.png";
+import tabAccountFocused from "@app/server/public/tab_account_focused.png";
 import tabCalendar from "@app/server/public/tab_calendar.png";
+import tabCalendarFocused from "@app/server/public/tab_calendar_focused.png";
 import tabHome from "@app/server/public/tab_home.png";
+import tabHomeFocused from "@app/server/public/tab_home_focused.png";
 import tabMission from "@app/server/public/tab_mission.png";
+import tabMissionFocused from "@app/server/public/tab_mission_focused.png";
 import { BottomTabBar } from "@react-navigation/bottom-tabs";
 import { BlurView } from "expo-blur";
 import { Image } from "expo-image";
@@ -46,7 +50,7 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "",
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ focused }) => (
             <View
               style={{
                 flexDirection: "column",
@@ -59,15 +63,15 @@ export default function TabsLayout() {
               {/* <TabBarIcon name="home" color={color} size={24} /> */}
               <Image
                 style={{ width: 34, height: 34 }}
-                source={tabHome}
+                source={focused ? tabHomeFocused : tabHome}
                 contentFit="contain"
               />
               <Text
                 style={{
                   marginTop: 5,
                   fontSize: 8,
-                  color: "#8F9092",
-                  fontWeight: 600,
+                  color: focused ? "#7FB3E8" : "#8F9092",
+                  fontWeight: focused ? 700 : 600,
                 }}
               >
                 HOME
@@ -81,7 +85,7 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "",
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ focused }) => (
             <View
               style={{
                 flexDirection: "column",
@@ -94,15 +98,15 @@ export default function TabsLayout() {
               {/* <TabBarIcon name="home" color={color} size={24} /> */}
               <Image
                 style={{ width: 34, height: 34 }}
-                source={tabCalendar}
+                source={focused ? tabCalendarFocused : tabCalendar}
                 contentFit="contain"
               />
               <Text
                 style={{
                   marginTop: 5,
                   fontSize: 8,
-                  color: "#8F9092",
-                  fontWeight: 600,
+                  color: focused ? "#7FB3E8" : "#8F9092",
+                  fontWeight: focused ? 700 : 600,
                 }}
               >
                 CALENDAR
@@ -116,7 +120,7 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "",
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ focused }) => (
             <View
               style={{
                 flexDirection: "column",
@@ -129,15 +133,15 @@ export default function TabsLayout() {
               {/* <TabBarIcon name="home" color={color} size={24} /> */}
               <Image
                 style={{ width: 34, height: 34 }}
-                source={tabAccount}
+                source={focused ? tabAccountFocused : tabAccount}
                 contentFit="contain"
               />
               <Text
                 style={{
                   marginTop: 5,
                   fontSize: 8,
-                  color: "#8F9092",
-                  fontWeight: 600,
+                  color: focused ? "#7FB3E8" : "#8F9092",
+                  fontWeight: focused ? 700 : 600,
                 }}
               >
                 ACCOUNT
@@ -151,7 +155,7 @@ export default function TabsLayout() {
         options={{
           headerShown: false,
           title: "",
-          tabBarIcon: ({ color }) => (
+          tabBarIcon: ({ focused }) => (
             <View
               style={{
                 flexDirection: "column",
@@ -164,15 +168,15 @@ export default function TabsLayout() {
               {/* <TabBarIcon name="home" color={color} size={24} /> */}
               <Image
                 style={{ width: 34, height: 34 }}
-                source={tabMission}
+                source={focused ? tabMissionFocused : tabMission}
                 contentFit="contain"
               />
               <Text
                 style={{
                   marginTop: 5,
                   fontSize: 8,
-                  color: "#8F9092",
-                  fontWeight: 600,
+                  color: focused ? "#7FB3E8" : "#8F9092",
+                  fontWeight: focused ? 700 : 600,
                 }}
               >
                 MISSION
